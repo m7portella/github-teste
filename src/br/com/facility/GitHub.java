359152460ef0ac02e01b5b268799bbb6a49f2648
Marcelo Portella <m7portella@gmail.com>
2014-07-09 11:01:25 -0300

    Criação de classe Cliente e teste

--- src/br/com/facility/GitHub.java
+++ src/br/com/facility/GitHub.java
@@ -1,9 +1,16 @@
 package br.com.facility;
 
+import br.com.facility.to.Cliente;
+
 public class GitHub {
 
 	public static void main(String[] args) {
 		//teste de commit
+		
+		Cliente c = new Cliente();
+		c.setNome("Marcelo");
+		
+		System.out.println(c.getNome());
 	}
 	
 }
---
+++ src/br/com/facility/to/Cliente.java
@@ -0,0 +1,38 @@
+package br.com.facility.to;
+
+import java.util.Calendar;
+
+public class Cliente {
+	
+	private int id;
+	
+	private String nome;
+	
+	private Calendar dtNascimento;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Calendar getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Calendar dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	
+
+}
